4ec44c777151e1fd9f57cfb82149e5239d626d04
Chris Schalago <chrisschalago@gmail.com>
2018-01-29 14:53:06 -0500

    Implement basic login with sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Twitlike!"
       redirect_to user_url(@user)
     else
