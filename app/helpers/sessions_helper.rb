28d8cfd6dcb42395511b0c7016a0c89c4239af84
Chris Schalago <chrisschalago@gmail.com>
2018-01-29 22:17:26 -0500

    Implement remember_token, remember_digest in db, and cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,16 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any)
   def current_user
-    @current_user = @current_user || User.find_by(id: session[:user_id])
-    # @current_user ||= User.find_by(id: session[:user_id]) IDIOMATIC WAY IN RUBY
+    if session[:user_id]
+      @current_user = @current_user || User.find_by(id: session[:user_id])
+      # @current_user ||= User.find_by(id: session[:user_id]) IDIOMATIC WAY IN RUBY
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Returns true if user is logged-in
@@ -16,8 +24,22 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def remember(user)
+    user.remember # makes new token and stores the digest on db
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
