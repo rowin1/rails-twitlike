90cacbe72222064431f56f8ed30706365d64a207
Chris Schalago <chrisschalago@gmail.com>
2018-02-03 16:59:52 -0500

    Add Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy # when user is destroyed, so are its microposts
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -43,6 +53,21 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Follows another user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows another user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
