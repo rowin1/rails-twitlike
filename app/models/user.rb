66cbad6280e38fda5afd32253eb41bc68f2d06c3
Chris Schalago <chrisschalago@gmail.com>
2018-02-04 18:43:56 -0500

    Update Feed method to use SQL subselect for better scaling

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,10 @@ class User < ApplicationRecord
 
   # Gets called by the Static Pages Controller
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # Returns hash digest of the given string
