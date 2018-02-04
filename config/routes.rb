c4e17778dcb9da0e32b988f16c613681eeadb8b3
Chris Schalago <chrisschalago@gmail.com>
2018-02-04 02:15:11 -0500

    Add 'following' and 'followers' pages to User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,14 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
